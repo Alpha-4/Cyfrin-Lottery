9e9bf2e01603ead837553d58a01e9c2916032359
Alpha-4 <aarondadhwal@gmail.com>
2025-04-21 22:02:04 +0530

    added dependency and remapping for dependency

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,21 +10,36 @@ contract Raffle {
     /*Errors here */
     Error Raffle__NotEnoughEth();
 
-    uint256 private immutable i_entranceFee;
+    /*Events here */
+    event RaffleJoined(address indexed player);
 
-    constructor(uint256 _entranceFee) {
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players;
+
+    constructor(uint256 _entranceFee, uint256 _interval) {
         i_entranceFee = _entranceFee;
+        i_interval=_interval;
+        s_lastTimeStamp= block.timestamp;
     }
 
-    function enterRaffle() public {
+    function enterRaffle() external {
         //require(msg.value >= i_entranceFee,"Amount is less than the entranceFee");//Not gas efficient as String has to be stored
         //require(msg.value >= i_entranceFee, NotEnoughEth()); //Still not gas efficient and valid on new verions
         if(msg.value < i_entranceFee) revert Raffle__NotEnoughEth();
+        s_players.push(payable(msg.sender));
+        
+        emit RaffleJoined(msg.sender); 
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if(block.timestamp - s_lastTimeStamp > i_interval){
 
-    function getEntranceFee() public returns (uint256) {
+        }
+    }
+
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
